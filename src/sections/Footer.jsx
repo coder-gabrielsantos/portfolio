7f4fedb1008d81d01880379002f9ea2abddcce50
Gabriel Santos <coder.gabrielsantos@gmail.com>
2025-12-03 14:03:44 -0300

    feat(footer): display current year dynamically

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,24 +1,24 @@
 import React from "react";
-
 import Logo from "../util/Logo.svg";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
 
-    return (
-        <footer
-            className="section-footer" id="footer">
+  return (
+    <footer className="section-footer" id="footer">
+      <div className="container center-text">
+        <div className="logo-column">
+          <a className="footer-logo container" href="#">
+            <img className="logo" src={Logo} alt=""/>
+          </a>
 
-            <div className="container center-text">
-                <div className="logo-column">
-                    <a className="footer-logo container" href="#">
-                        <img className="logo" src={Logo} alt=""/>
-                    </a>
-
-                    <p className="copyright">Copyright &copy; 2023 by Gabriel Santos</p>
-                </div>
-            </div>
-        </footer>
-    );
+          <p className="copyright">
+            Copyright &copy; {currentYear} by Gabriel Santos
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
